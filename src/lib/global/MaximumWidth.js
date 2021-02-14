0a1c5787401a1a7dd2022e17f08b731697683756
GannyK1 <73115423+GannyK1@users.noreply.github.com>
2021-02-14 20:32:44 +0100

    changed product atributes, theme and buttons

---
+++ src/lib/global/MaximumWidth.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+
+const MaximumWidthBox = styled.div `
+max-width: 1300px; 
+`
+
+const MaximumWidth = () => {
+    return (
+        <MaximumWidthBox>
+            
+        </MaximumWidthBox>
+    )
+}
+
+export default MaximumWidth
